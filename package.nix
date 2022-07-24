45a53df4c09418d54af83206f2cf181ef8ddfafe
Tal Zion <talzion12@gmail.com>
2022-07-24 16:58:02 +0300

    updated to 394

--- package.nix
+++ package.nix
@@ -1,21 +1,20 @@
 { stdenv
 , fetchurl
-, autoPatchelfHook,
-...
+, ...
 }:
 
 stdenv.mkDerivation rec {
   pname = "gke-gcloud-auth-plugin";
-  version = "393.0.0";
+  version = "394.0.0-0";
+  sha256 = "8ca433e924fe120d231de5047344c7c76dc2efed716314953dbfebe9b584d9c1";
+  arch = "amd64";
 
+  # get updates from here:
+  # https://packages.cloud.google.com/apt/dists/cloud-sdk/main/binary-amd64/Packages
   src = fetchurl {
-    url = "https://packages.cloud.google.com/apt/pool/google-cloud-cli-gke-gcloud-auth-plugin_${version}-0_amd64_04cca5f6ba7035954886226c519fa3627f97750bb664371b3def0864d6889e92.deb";
-    sha256 = "sha256-BMyl9rpwNZVIhiJsUZ+jYn+XdQu2ZDcbPe8IZNaInpI=";
+    url = "https://packages.cloud.google.com/apt/pool/google-cloud-cli-gke-gcloud-auth-plugin_${version}_${arch}_${sha256}.deb";
+    sha256 = sha256;
   };
-
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
 
   unpackPhase = ''
     cp $src $TMP/${pname}.deb
