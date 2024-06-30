e67a42740702f740c6b68718418b07ff723b1586
Tal Zion <talzion12@gmail.com>
2024-06-30 10:45:19 +0300

    Updated to gke-gcloud-auth-plugin-482.0.0-0 and fixed the auto-update pipeline

--- package.nix
+++ package.nix
@@ -5,14 +5,15 @@
 
 stdenv.mkDerivation rec {
   pname = "gke-gcloud-auth-plugin";
-  version = "480.0.0-0";
-  sha256 = "b9ab1d2a734c53f8abd3ebd651be544c6fdd2d93f3b988ebf15b7186fc55d2bc";
+  version = "482.0.0-0";
+  sha256 = "092ec6c9dbab7d73bb0150dd6008d022bbe20d8d8695c9ccca1f175c12cdae62";
+  md5sum = "a01786b4884d225436ee43a25cf76183";
   arch = "amd64";
 
   # get updates from here:
   # https://packages.cloud.google.com/apt/dists/cloud-sdk/main/binary-amd64/Packages
   src = fetchurl {
-    url = "https://packages.cloud.google.com/apt/pool/google-cloud-cli-gke-gcloud-auth-plugin_${version}_${arch}_${sha256}.deb";
+    url = "https://packages.cloud.google.com/apt/pool/cloud-sdk/google-cloud-cli-gke-gcloud-auth-plugin_${version}_${arch}_${md5sum}.deb";
     sha256 = sha256;
   };
 
